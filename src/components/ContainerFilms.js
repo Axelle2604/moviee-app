4da1c3105497f3b2e9ee27622e40f8070f10bc53
Clemo2604 <axelle.dru@stackerine.com>
2019-03-27 15:41:14 +0100

    Home film container & stylizing

---
+++ src/components/ContainerFilms.js
@@ -0,0 +1,36 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import Film from './Film.js';
+import { translateWord } from './utils/changeLang';
+import { withLangage } from './utils/withLangage';
+import {
+  FilmsContainer,
+  FilmsSubContainer,
+} from './styled-components/containerFilmsStyled';
+
+const ContainerFilms = ({ langValue, listFilms }) => (
+  <FilmsContainer>
+    <h2>{translateWord('newReleases', langValue)}</h2>
+    <FilmsSubContainer>
+      {listFilms.map(({ poster_path, title, id, release_date }) => (
+        <Film
+          image={poster_path}
+          title={title}
+          key={id}
+          id={id}
+          date={release_date}
+        />
+      ))}
+    </FilmsSubContainer>
+  </FilmsContainer>
+);
+
+export default withLangage(memo(ContainerFilms));
+
+ContainerFilms.propTypes = {
+  listFilms: PropTypes.array,
+  poster_path: PropTypes.string,
+  title: PropTypes.string,
+  id: PropTypes.number,
+  release_date: PropTypes.string,
+};
---
+++ src/components/styled-components/containerFilmsStyled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const FilmsContainer = styled.div`
+  margin-top: 40px;
+  margin-bottom: 40px;
+  &h2 {
+    font-weight: 800;
+  }
+`;
+
+export const FilmsSubContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
