fcb828f763803cc9c682e0401a2f33e87f3bef1a
Clemo2604 <axelle.dru@stackerine.com>
2019-03-27 15:42:15 +0100

    Footer & stylizing

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React, { memo } from 'react';
+import { FooterContainer } from './styled-components/footerStyled';
+
+const Footer = () => {
+  return <FooterContainer>Axelle DRU - 2019</FooterContainer>;
+};
+
+export default memo(Footer);
---
+++ src/components/styled-components/footerStyled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  border-top: solid 1px rgb(238, 238, 238);
+  padding: 20px;
+  margin-top: 20px;
+`;
